d9f54745a15c112f604237c3b2a41b8aaf1d51e9
Amanpreet Kaur <amanpreet8507@gmail.com>
2024-01-23 12:49:19 -0800

    fetch api data, use of use state, useEffect

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -1,8 +1,21 @@
-function Comment({ name, comment }) {
+import { useEffect, useState } from 'react'
+import './Comment.css'
+function Comment({ name, comment, likes }) {
+    const [like, setLike] = useState(false)
+    const [likeCount, setLikeCounts] = useState(likes-1)
+
+    useEffect(()=>{
+        setLikeCounts(likeCount + 1)
+    }, [like])
+
     return (
-        <div>
+        <div className="comments">
             <p>{name}</p>
             <p>{comment}</p>
+            <p>Likes: {likeCount}</p>
+            <button onClick={()=>{
+                setLike(!like)
+            }}><i> {like ? "♥️" : "like"}</i></button>
         </div>
     )
 }
--- src/components/CommentList/CommentList.js
+++ src/components/CommentList/CommentList.js
@@ -1,12 +1,27 @@
+import axios from "axios";
 import Comment from "../Comment/Comment";
+import { useEffect, useState } from "react";
 
 function CommentList() {
   const comments = [];
+  const [commentList, setCommentList] = useState(comments)
+
+  const fetchCommentsList = async () => {
+    const commentsData = await axios.get('https://project-1-api.herokuapp.com/comments?api_key=0c1318a1-0c91-4df5-9cfa-b8cbad39d045')
+    setCommentList(commentsData.data)
+  }
+  
+
+  useEffect(() =>{
+    fetchCommentsList()
+  },[])
+
+  console.log('update comments list')
 
   return (
     <div>
-      {comments.map((comment) => {
-        return <Comment name={comment.name} comment={comment.comment} />;
+      {commentList.map((comment) => {
+        return <Comment key={comment.id} name={comment.name} comment={comment.comment} likes={comment.likes} />;
       })}
     </div>
   );
