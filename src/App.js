5a18f16bdc31457296a5ed9ad40dd83d7df8ad4b
Amanpreet Kaur <amanpreet8507@gmail.com>
2024-01-23 11:58:41 -0800

    add boiler code

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import CommentList from './components/CommentList/CommentList';
+
+function App() {
+    return (
+        <div>
+            <h1 className="header">
+                Comments
+            </h1>
+            <CommentList />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/CommentList/CommentList.js
@@ -0,0 +1,15 @@
+import Comment from "../Comment/Comment";
+
+function CommentList() {
+  const comments = [];
+
+  return (
+    <div>
+      {comments.map((comment) => {
+        return <Comment name={comment.name} comment={comment.comment} />;
+      })}
+    </div>
+  );
+}
+
+export default CommentList;
